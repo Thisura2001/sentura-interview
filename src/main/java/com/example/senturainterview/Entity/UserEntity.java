cbeb37c8cd65c02e03f035b13c102987b14bbdc5
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2025-04-04 09:25:58 +0530

    add jpa and create dto and entity

--- src/main/java/com/example/senturainterview/Entity/UserEntity.java
+++ src/main/java/com/example/senturainterview/Entity/UserEntity.java
@@ -1,5 +1,7 @@
 package com.example.senturainterview.Entity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
@@ -9,9 +11,9 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class UserEntity {
-
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String uid;
-
     private String name;
 }
---
+++ src/main/java/com/example/senturainterview/Repository/UserRepo.java
@@ -0,0 +1,7 @@
+package com.example.senturainterview.Repository;
+
+import com.example.senturainterview.Entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<UserEntity,long> {
+}
