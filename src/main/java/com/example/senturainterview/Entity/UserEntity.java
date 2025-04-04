fc75881ebbbe5b4eedec69e3952940e5182f7296
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2025-04-04 09:21:01 +0530

    create entity

--- src/main/java/com/example/senturainterview/Entity/UserEntity.java
+++ src/main/java/com/example/senturainterview/Entity/UserEntity.java
@@ -1,4 +1,17 @@
 package com.example.senturainterview.Entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserEntity {
+
+    @Id
+    private String uid;
+
+    private String name;
 }
